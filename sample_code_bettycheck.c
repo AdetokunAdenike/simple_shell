c64bcf29ae1ea7ebe6724e50a76df2b0432508e7
AdetokunAdenike <adetokunadenike@gmail.com>
2024-08-07 18:15:25 +0100

    Update

--- sample_code_bettycheck.c
+++ sample_code_bettycheck.c
@@ -12,9 +12,9 @@ int factorial(int n)
 	{
 		return (-1); /* Error case for negative numbers */
 	}
-if (n == 0 || n == 1)
+	if (n == 0 || n == 1)
 	{
-		return 1; /* Base case: factorial of 0 or 1 is 1 */
+		return (1); /* Base case: factorial of 0 or 1 is 1 */
 	}
 	return (n * factorial(n - 1)); /* Recursive case */
 }
